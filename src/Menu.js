bffa682d3eb322d71d43acbde815240a7252b5dd
ggggabe <who@gabe.is>
2021-06-15 00:04:34 -0400

    New drop

--- src/Menu.js
+++ src/Menu.js
@@ -95,7 +95,7 @@ const AnimatedLink = ({ href, name, index }) => {
 export default () => {
   const menu = [
     { name: 'Apple Music', href: 'https://music.apple.com/gr/artist/alec-meza/id1500249995' },
-    { name: 'Spotify', href: 'https://open.spotify.com/artist/3zTBUhiJEIBJTsFiuWJcik' },
+    { name: 'Spotify', href: 'https://open.spotify.com/track/6oOUi8dRq37vv7yvIlkhSs' },
     { name: 'SoundCloud', href: 'https://soundcloud.com/alec-meza/falling-demo/s-Q1aEhjtSKrn' },
     { name: 'YouTube', href: 'https://youtu.be/Ama9PFPpwU8' },
     { name: 'Instagram', href: 'https://www.instagram.com/_alecmeza/' },
