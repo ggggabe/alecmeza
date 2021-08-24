5397e95514771c1df8f5ad6ec8f41844d3073885
ggggabe <who@gabe.is>
2021-08-24 16:27:37 -0400

    add shop link

--- src/Menu.js
+++ src/Menu.js
@@ -78,7 +78,7 @@ function useHover() {
   return [ref, currentPadding]
 }
 
-const AnimatedLink = ({ href, name, index }) => {
+const AnimatedLink = ({ href, name }) => {
   const [hoverRef, padding] = useHover()
   const [color, setColor] = useState('#eee')
 
@@ -92,19 +92,16 @@ const AnimatedLink = ({ href, name, index }) => {
   </li>
 }
 
-export default () => {
-  const menu = [
-    { name: 'Apple Music', href: 'https://music.apple.com/us/album/when-youre-ready-single/1569414500' },
-    { name: 'Spotify', href: 'https://open.spotify.com/track/6oOUi8dRq37vv7yvIlkhSs' },
-    { name: 'SoundCloud', href: 'https://soundcloud.com/alec-meza/falling-demo/s-Q1aEhjtSKrn' },
-    { name: 'YouTube', href: 'https://youtu.be/dm3_UWk_1KA' },
-    { name: 'Instagram', href: 'https://www.instagram.com/_alecmeza/' },
-    { name: 'Twitter', href: 'https://twitter.com/_alecmeza' }
-  ]
+const menu = [
+  { name: 'Apple Music', href: 'https://music.apple.com/us/album/when-youre-ready-single/1569414500' },
+  { name: 'Spotify', href: 'https://open.spotify.com/track/6oOUi8dRq37vv7yvIlkhSs' },
+  { name: 'SoundCloud', href: 'https://soundcloud.com/alec-meza/falling-demo/s-Q1aEhjtSKrn' },
+  { name: 'YouTube', href: 'https://youtu.be/dm3_UWk_1KA' },
+  { name: 'Instagram', href: 'https://www.instagram.com/_alecmeza/' },
+  { name: 'Twitter', href: 'https://twitter.com/_alecmeza' },
+  { name: 'Shop', href: 'https://marsadrecords.com/archives.ev/' },
+]
 
-  return (
-    <ul>
-      {menu.map((props, i) => <AnimatedLink key={i} {...props} index={i} />)}
-    </ul>
-  )
-}
+export default () => <ul>
+  {menu.map(props => <AnimatedLink key={props.name} {...props}/>)}
+</ul>
